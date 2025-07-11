685381e3ee2481c98178b74dfbd822184d5f2dc1
peberlin@silabs.com <peberlin@silabs.com>
2025-07-11 13:43:56 +0200

    [NOJIRA]: Add the first fun file (deepcopy for now)

---
+++ Fun_with_deepcopy.py
@@ -0,0 +1,39 @@
+import copy
+from enum import Enum
+
+class SkillLevel(Enum):
+    BEGINNER = "beginner"
+    INTERMEDIATE = "intermediate"
+    ADVANCED = "advanced"
+
+class Hobby:
+    def __init__(self, name, skill_level):
+        self.name = name
+        self.skill_level = skill_level
+
+    def __repr__(self):
+        return f"Hobby(name={self.name!r}, skill_level={self.skill_level!r})"
+
+class Person:
+    def __init__(self, name, hobbies, main_hobby=None):
+        self.name = name
+        self.hobbies = hobbies
+        self.main_hobby = main_hobby
+
+
+    def __repr__(self):
+        return f"Person(name={self.name!r}, hobbies={self.hobbies!r}, main_hobby={self.main_hobby!r})"
+
+# Create an instance
+original = Person("Alice", ["reading", "cycling"], main_hobby=Hobby("swimming", SkillLevel.INTERMEDIATE))
+
+# Make a deepcopy
+cloned = copy.deepcopy(original)
+
+# Modify the clone
+cloned.name = "Bob"
+cloned.hobbies.append("painting")
+cloned.main_hobby.skill_level = SkillLevel.ADVANCED
+
+print("Original:", original)
+print("Cloned:  ", cloned)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # silabs_python_playground
 Playground for daily work
+## Just a fun project to put some code in and learn github.
+I should not have added the above line as a title....
